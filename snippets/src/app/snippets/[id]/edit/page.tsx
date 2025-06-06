d05ba1f16fabdd8d363873458d4bd3c2b40d4ba4
caofontaine <benjamin.cao88@gmail.com>
2025-06-06 18:45:35 -0400

    add edit code

--- snippets/src/app/snippets/[id]/edit/page.tsx
+++ snippets/src/app/snippets/[id]/edit/page.tsx
@@ -1,3 +1,7 @@
+import { notFound } from "next/navigation";
+import { db } from "@/db";
+import SnippetEditForm from "@/components/snippet-edit-form";
+
 interface SnippetEditPageProps {
   params: Promise<{
     id: string;
@@ -7,5 +11,18 @@ interface SnippetEditPageProps {
 export default async function SnippetEditPage(props: SnippetEditPageProps) {
   const { id } = await props.params;
 
-  return <div>Editing snippet with id {id}</div>;
+  const snippetId = parseInt(id);
+  const snippet = await db.snippet.findFirst({
+    where: { id: snippetId },
+  });
+
+  if (!snippet) {
+    return notFound();
+  }
+
+  return (
+    <div>
+      <SnippetEditForm snippet={snippet} />
+    </div>
+  );
 }
