ad7ae14bc906c1737f0bdb785ba115c455facf8f
Ben Cao <bcao@Bens-MacBook-Pro.local>
2025-11-03 21:06:57 -0500

    update src

--- snippets/src/components/snippet-edit-form.tsx
+++ snippets/src/components/snippet-edit-form.tsx
@@ -1,8 +1,9 @@
 "use client";
 
-import type { Snippet } from "@prisma/client";
+import type { Snippet } from "@prisma/client"
 import Editor from "@monaco-editor/react";
 import { useState } from "react";
+import * as actions from "@/actions";
 
 interface SnippetEditFormProps {
   snippet: Snippet;
@@ -14,6 +15,8 @@ export default function SnippetEditForm({ snippet }: SnippetEditFormProps) {
   const handleEditorChange = (value: string = "") => {
     setCode(value);
   };
+
+  const editSnippetAction = actions.editSnippet.bind(null, snippet.id, code);
 
   return (
     <div>
@@ -27,6 +30,11 @@ export default function SnippetEditForm({ snippet }: SnippetEditFormProps) {
         }}
         onChange={handleEditorChange}
       />
+      <form action={editSnippetAction}>
+        <button type="submit" className="p-2 border rounded">
+          Save
+        </button>
+      </form>
     </div>
   );
 }
