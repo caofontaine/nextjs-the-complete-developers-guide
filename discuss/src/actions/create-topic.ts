fd086224a770c8ebe9dffa09d48430632dfb0e79
Ben Cao <benjamin.cao88@gmail.com>
2025-12-19 11:28:22 -0500

    add create topic form validation and initial form state setup

--- discuss/src/actions/create-topic.ts
+++ discuss/src/actions/create-topic.ts
@@ -1,5 +1,27 @@
 'use server';
 
-export async function createTopic() {
+import { z } from 'zod';
+
+const createTopicSchema = z.object({
+  name: z
+    .string()
+    .min(3)
+    .regex(/^[a-z-]+$/, { 
+      message: 'Must be lowercase letters for dashes without spaces'
+    }),
+  description: z.string().min(10),
+});
+
+export async function createTopic(formState: number, formData: FormData) {
+  const result = createTopicSchema.safeParse({
+    name: formData.get('name'),
+    description: formData.get('description')
+  });
+
+  if(!result.success) {
+    //result.error.flatten().fieldErrors
+  };
+
+  return 10;
   // TODO: revalidate the homepage
 }
--- discuss/src/components/topics/topic-create-form.tsx
+++ discuss/src/components/topics/topic-create-form.tsx
@@ -1,3 +1,6 @@
+'use client';
+
+import { useActionState } from 'react';
 import {
   Input,
   Button,
@@ -8,18 +11,22 @@ import {
 } from '@nextui-org/react';
 import * as actions from '@/actions';
 
+
 export default function TopicCreateForm() {
+  const [formState, action] = useActionState(actions.createTopic, 5);
+
   return (
     <Popover placement="left">
       <PopoverTrigger>
         <Button color="primary">Create a Topic</Button>
       </PopoverTrigger>
       <PopoverContent>
-        <form action={actions.createTopic}>
+        <form action={action}>
           <div className="flex flex-col gap-4 p-4 w-80">
             <h3 className="text-log">Create a Topic</h3>
-            <Input label="Name" labelPlacement="outside" placeholder="Name" />
+            <Input name="name" label="Name" labelPlacement="outside" placeholder="Name" />
             <Textarea
+              name="description"
               label="Description"
               labelPlacement="outside"
               placeholder="Describe your topic"
