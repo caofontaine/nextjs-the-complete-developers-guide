e110ab88db1ea2d688d643860bfdd26ce95608be
Ben Cao <benjamin.cao88@gmail.com>
2026-01-03 14:36:20 -0500

    add memoization

--- discuss/src/db/queries/comments.ts
+++ discuss/src/db/queries/comments.ts
@@ -1,11 +1,12 @@
 import type { Comment } from "@prisma/client";
+import { cache } from 'react';
 import { db } from "@/db";
 
 export type CommentWithAuthor = Comment &
   { user: { name: string | null, image: string | null };
 };
 
-export function fetchCommentsByPostId(postId: string): Promise<CommentWithAuthor[]> {
+export const fetchCommentsByPostId = cache((postId: string): Promise<CommentWithAuthor[]> => {
  return db.comment.findMany({
   where: { postId },
   include: {
@@ -17,4 +18,4 @@ export function fetchCommentsByPostId(postId: string): Promise<CommentWithAuthor
     }
   }
  });
-}
+});
